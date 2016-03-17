89e054879e71c1761e0d3161da4302df5fe6fc58
Bedri <bsendir1@binghamton.edu>
2016-03-17 16:55:34 -0400

    Update runclean.sh

--- runclean.sh
+++ runclean.sh
@@ -1,33 +1,45 @@
-#github
-#workload size
 #!/bin/bash +vx
+#hardcoded parameters
 HADOOP_HOME=/var/work/shizir1/hadoop-1.2.1
 SPARK_HOME=/var/work/shizir1/spark-1.6.0-bin-hadoop1
 HIBENCH_HOME=/var/work/shizir1/HiBench
 timestamp=$(date +%s)
 
-$HADOOP_HOME/bin/stop-all.sh
-$SPARK_HOME/sbin/stop-all.sh
+if [ "$#" -ne 3 ]; then
+    tput setaf 1;echo "Usage ./runclean [list of machines]  [commands to run on remote machines] [number of hi-bench iterations]";tput sgr0;
+    exit;
+fi
 
-# Sanirim buarkadas ve hadoop format (commands'in icindeki) calismiyor. Yarin bakariz.
-cp -r $HIBENCH_HOME/report/hibench.report $HIBENCH_HOME/hibench.report_$timestamp
+for ((iter = 1; iter <= $3; iter++));
+do
+	echo "Running $3 iterations"
+	###### ---> stop hadoop/spark
+	$HADOOP_HOME/bin/stop-all.sh
+	$SPARK_HOME/sbin/stop-all.sh
 
-for machines in `cat $1`
-do 
-   #for loop 1 - 10
-   #hibench reportu yedekle
-   #clean
-   ssh $machines 'bash -s' < $2
+	####### ---> run
+	for machines in `cat $1`
+	do
+   		ssh $machines 'bash -s' < $2
+	done
+
+	sleep 10
+
+	#### ---> start hadoop/spark
+	$HADOOP_HOME/bin/start-all.sh
+	$SPARK_HOME/sbin/start-all.sh
+
+	sleep 60
+
+	#### ---> start workload
+	echo "Starting workload...."
+	$HIBENCH_HOME/bin/run-all.sh
+
+	# Run ./generate command
+	echo "iteration $iter completed. Generating the report..."
+	#FIX command for generating graph
+
+	### ---> move results to public_html
+	mv -r $HIBENCH_HOME/report ~/public_html/report_$timestamp
+	chmod -R 755 ~/public_html
 done
-
-sleep 10
-
-#hadoop baslat ./start-all.sh
-$HADOOP_HOME/bin/start-all.sh
-$SPARK_HOME/sbin/start-all.sh
-
-sleep 60
-
-echo "---> Running the tests:"
-$HIBENCH_HOME/bin/run-all.sh
-
