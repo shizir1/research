a41811b3df24da440b1b45e7cdc41451f3d2aa92
Bedri <bsendir1@binghamton.edu>
2016-03-17 19:08:07 -0400

    Update runclean.sh

--- runclean.sh
+++ runclean.sh
@@ -5,14 +5,14 @@ SPARK_HOME=/var/work/shizir1/spark-1.6.0-bin-hadoop1
 HIBENCH_HOME=/var/work/shizir1/HiBench
 timestamp=$(date +%s)
 
-if [ "$#" -ne 3 ]; then
-    tput setaf 1;echo "Usage ./runclean [list of machines]  [commands to run on remote machines] [number of hi-bench iterations]";tput sgr0;
+if [ "$#" -ne 4 ]; then
+    tput setaf 1;echo "Usage ./runclean [list of machines]  [commands to run on remote machines] [workload prefix] [number of hi-bench iterations]";tput sgr0;
     exit;
 fi
 
-for ((iter = 1; iter <= $3; iter++));
+for ((iter = 1; iter <= $4; iter++));
 do
-	echo "Running $3 iterations"
+	echo "Running $4 iterations"
 	###### ---> stop hadoop/spark
 	$HADOOP_HOME/bin/stop-all.sh
 	$SPARK_HOME/sbin/stop-all.sh
@@ -37,9 +37,9 @@ do
 
 	# Run ./generate command
 	echo "iteration $iter completed. Generating the report..."
-	#FIX command for generating graph
-
+	$HIBENCH_HOME/bin/report_gen_plot.py $HIBENCH_HOME/report/hibench.report
+	
 	### ---> move results to public_html
-	mv $HIBENCH_HOME/report ~/public_html/report_$timestamp
+	mv $HIBENCH_HOME/report ~/public_html/report_$timestamp_$3_$4
 	chmod -R 755 ~/public_html
 done
